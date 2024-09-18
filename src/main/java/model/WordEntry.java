2e5060204490d55aa7fb100adfccafdbabe6263f
Leo Mühlböck <leo.muehlboeck@chello.at>
2024-09-18 16:37:17 +0200

    Add WordTrainer

--- src/main/java/model/WordEntry.java
+++ src/main/java/model/WordEntry.java
@@ -1,30 +1,62 @@
 package model;
 
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 
+/**
+ * Represents a word entry.
+ * @author Leo MÜhlböck
+ * @version 2024-09-18
+ */
 public class WordEntry {
     private String word;
     private String url;
 
+    /**
+     * Creates a new word entry with the given word and URL.
+     * @param word the word
+     * @param url the URL
+     */
+    public WordEntry(String word, String url) {
+        setWord(word);
+        setUrl(url);
+    }
+
+    /**
+     * Returns the word.
+     * @return the word
+     */
     public String getWord() {
         return word;
     }
 
+    /**
+     * Sets the word.
+     * @param word the word
+     */
     public void setWord(String word) {
-        if(word == null) throw new IllegalArgumentException();
+        if(word == null || word.isEmpty()) throw new IllegalArgumentException();
         this.word = word;
     }
 
+    /**
+     * Returns the URL.
+     * @return the URL
+     */
     public String getUrl() {
         return url;
     }
 
+    /**
+     * Sets the URL.
+     * @param url the URL
+     */
     public void setUrl(String url) {
         if(url == null) throw new IllegalArgumentException();
         try {
-            new URL(url);
-        } catch (MalformedURLException exc) {
+            new URL(url).toURI();
+        } catch (MalformedURLException | URISyntaxException exc) {
             throw new IllegalArgumentException();
         }
         this.url = url;
---
+++ src/main/java/model/WordTrainer.java
@@ -0,0 +1,132 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Represents a word trainer.
+ * @author Leo MÜhlböck
+ * @version 2024-09-18
+ */
+public class WordTrainer {
+    private List<WordEntry> entries;
+    private int selected;
+    private int numAsked;
+    private int numRight;
+
+    /**
+     * Default entries for the word trainer.
+     */
+    public static final List<WordEntry> defaultEntries = Arrays.asList(
+            new WordEntry("Elefant", "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Elephant_near_ndutu.jpg/640px-Elephant_near_ndutu.jpg")
+    );
+
+    /**
+     * Creates a new word trainer with the default entries.
+     */
+    public WordTrainer() {
+        this(defaultEntries, -1, 0, 0);
+    }
+
+    /**
+     * Creates a new word trainer with the given entries.
+     * @param entries the entries
+     * @param selected the selected entry
+     * @param numAsked the number of asked entries
+     * @param numRight the number of right answers
+     */
+    public WordTrainer(List<WordEntry> entries, int selected, int numAsked, int numRight) {
+        setEntries(entries);
+        this.selected = selected;
+        setNumAsked(numAsked);
+        setNumRight(numRight);
+    }
+
+    /**
+     * Returns the entries.
+     * @return the entries
+     */
+    public List<WordEntry> getEntries() {
+        return entries;
+    }
+
+    /**
+     * Sets the entries.
+     * @param entries the entries
+     */
+    public void setEntries(List<WordEntry> entries) {
+        if(entries == null) throw new IllegalArgumentException();
+        this.entries = entries;
+    }
+
+    /**
+     * Returns the number of asked entries.
+     * @return the number of asked entries
+     */
+    public int getNumAsked() {
+        return numAsked;
+    }
+
+    /**
+     * Sets the number of asked entries.
+     * @param numAsked the number of asked entries
+     */
+    public void setNumAsked(int numAsked) {
+        if(numAsked < 0) throw  new IllegalArgumentException();
+        this.numAsked = numAsked;
+    }
+
+    /**
+     * Returns the number of right answers.
+     * @return the number of right answers
+     */
+    public int getNumRight() {
+        return numRight;
+    }
+
+    /**
+     * Sets the number of right answers.
+     * @param numRight the number of right answers
+     */
+    public void setNumRight(int numRight) {
+        if(numRight < 0) throw new IllegalArgumentException();
+        this.numRight = numRight;
+    }
+
+    /**
+     * Returns the selected entry.
+     * @return the selected entry
+     */
+    public WordEntry selectRandomEntry() {
+        this.selected = (int)(Math.random() * entries.size());
+        return getSelectedEntry();
+    }
+
+    /**
+     * Returns the selected entry.
+     * @param index the index of the entry
+     * @return the selected entry
+     */
+    public WordEntry selectEntry(int index) {
+        this.selected = index;
+        return getSelectedEntry();
+    }
+
+    /**
+     * Returns the selected entry.
+     * @return the selected entry
+     */
+    public WordEntry getSelectedEntry() {
+        return this.entries.get(this.selected);
+    }
+
+    /**
+     * Checks if the given input is correct.
+     * @param input the input
+     * @return true if the input is correct, false otherwise
+     */
+    public boolean check(String input) {
+        return getSelectedEntry().getWord().equals(input);
+    }
+}
