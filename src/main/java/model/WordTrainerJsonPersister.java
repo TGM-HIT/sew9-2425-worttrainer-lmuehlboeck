0a08d0fa22f55ab63f386b3f9dabfd7f5bac244f
Leo Mühlböck <leo.muehlboeck@chello.at>
2024-09-20 19:20:03 +0200

    Implement JSON persistence

--- src/main/java/model/WordTrainerJsonPersister.java
+++ src/main/java/model/WordTrainerJsonPersister.java
@@ -1,15 +1,58 @@
 package model;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Represents a word trainer persister that saves and loads a word trainer to and from a JSON file.
+ * @author Leo Mühlböck
+ * @version 2024-09-20
+ */
 public class WordTrainerJsonPersister implements WordTrainerPersister {
-    
+    private final String path;
+    private final Gson gson;
 
-    @Override
-    public void save(WordTrainer trainer) {
-
+    /**
+     * Creates a new word trainer persister with the default path.
+     */
+    public WordTrainerJsonPersister() {
+        this("./word_trainer.json");
     }
 
+    /**
+     * Creates a new word trainer persister with the given path.
+     * @param path the path
+     */
+    public WordTrainerJsonPersister(String path) {
+        if(path == null) throw new IllegalArgumentException();
+        this.path = path;
+        GsonBuilder builder = new GsonBuilder();
+        builder.setPrettyPrinting();
+        this.gson = builder.create();
+    }
+
+    /**
+     * Saves the given word trainer to the JSON file.
+     * @param trainer the word trainer
+     */
+    @Override
+    public void save(WordTrainer trainer) {
+        try {
+            gson.toJson(trainer, new FileWriter(this.path));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Loads the word trainer from the JSON file.
+     * @return the word trainer
+     */
     @Override
     public WordTrainer load() {
-        return null;
+        return gson.fromJson(path, WordTrainer.class);
     }
 }
--- src/main/java/model/WordTrainerPersister.java
+++ src/main/java/model/WordTrainerPersister.java
@@ -1,6 +1,20 @@
 package model;
 
+/**
+ * Represents a word trainer persister.
+ * @author Leo Mühlböck
+ * @version 2024-09-20
+ */
 public interface WordTrainerPersister {
+    /**
+     * Saves the given word trainer.
+     * @param trainer the word trainer
+     */
     void save(WordTrainer trainer);
+
+    /**
+     * Loads the word trainer.
+     * @return the word trainer
+     */
     WordTrainer load();
 }
