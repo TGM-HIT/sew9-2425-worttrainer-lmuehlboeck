d852b4181ef208634091c7c9df68abed8c1c525e
Benjamin Edlinger <bedlinger@student.tgm.ac.at>
2024-09-30 11:21:02 +0200

    Add option to choose persistence

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,17 @@
 import controller.Controller;
+import model.WordTrainerJsonPersister;
+import model.WordTrainerPersister;
+import model.WordTrainerXmlPersister;
 
 import javax.swing.*;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        new Controller();
+        Map<String, WordTrainerPersister> persisters = Map.of("JSON", new WordTrainerJsonPersister(), "XML", new WordTrainerXmlPersister());
+        String chosenPerisiter = (String) JOptionPane.showInputDialog(null, "Which chosenPerisiter do you want to use?",
+                "Persister", JOptionPane.QUESTION_MESSAGE, null, persisters.keySet().toArray(), "JSON");
+        new Controller(persisters.get(chosenPerisiter));
     }
 }
